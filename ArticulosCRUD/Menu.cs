ce86267cbf07c7a00d6cab89fab55dabcaffd2e3
cristopher3454O <cristo654cabl3@gmail.com>
2026-08-27 11:38:16 -0600

    Logica inicial del menu

--- ArticulosCRUD/Menu.cs
+++ ArticulosCRUD/Menu.cs
@@ -25,6 +25,11 @@ namespace ArticulosCRUD
                 switch (opcion)
                 {
                     case "0": continuar = false; break;
+                    case "1": MostrarAgregar(); break;
+                    case "2": MostrarListar(); break;
+                    case "3": MostrarBuscar(); break;
+                    case "4": MostrarModificar(); break;
+                    case "5": MostrarEliminar(); break;
                     default :
                         Console.WriteLine("Opcion invalida");
                         Console.ReadLine();
@@ -38,6 +43,30 @@ namespace ArticulosCRUD
         {
             Console.Clear();
             Console.WriteLine("Opción Agregar Seleccionada");
+            Console.ReadLine();
+        }
+        public void MostrarListar()
+        {
+            Console.Clear();
+            Console.WriteLine("Opción Listar Seleccionada");
+            Console.ReadLine();
+        }
+        public void MostrarBuscar()
+        {
+            Console.Clear();
+            Console.WriteLine("Opción Buscar Seleccionada");
+            Console.ReadLine();
+        }
+        public void MostrarModificar()
+        {
+            Console.Clear();
+            Console.WriteLine("Opción Modifcar Seleccionada");
+            Console.ReadLine();
+        }
+        public void MostrarEliminar()
+        {
+            Console.Clear();
+            Console.WriteLine("Opción Eliminar Seleccionada");
             Console.ReadLine();
         }
     }
